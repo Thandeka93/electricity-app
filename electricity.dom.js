ad7944115621c14c79eaec9f9fcd2c6f4f75ac81
Thandeka93 <noluthandombali4@gmail.com>
2023-05-30 12:19:08 +0200

    tests

--- electricity.dom.js
+++ electricity.dom.js
@@ -1,9 +1,72 @@
 // DOM element(s) references
-
-
+//const btn topupNow mt1 = document.querySelector(".btn topupNow mt1")
+//const button = document.querySelector('.btn.topupNow.mt1');
+const topupButton = document.querySelector('.btn.topupNow.mt1');
+const useButton = document.querySelector('.btn.useNow.mt1');
+//const button = document.querySelector('.btn useNow mt1')
+ //const unitsAvailable = document.querySelector(".unitsAvailable")
 
 // Factory Function instance 
 const electricity =  Electricity();
 
+//declaring variables 
+let unitsAvailable = 0;
+let totalUnits = 0;
+let totalAmount = 0;
+let advanceUsed = false;
+let advanceAmount = 30;
+let advanceUnits = 21;
 
-// DOM events here 
+//function to handle top-up
+function topUpElectricity(amount) {
+    var amount = document.querySelector("input[name='buyElectricity']:checked");
+
+    if (amount === 10) {
+        unitsAvailable+= 7;
+    } else if (amount === 20) {
+        unitsAvailable += 14;
+    } else if (amount === 50) {
+        unitsAvailable += 35;
+    }
+  
+    totalUnits += unitsAvailable;
+    totalAmount += amount;
+    updateDisplay();
+  }
+  topupButton .addEventListener("click", topUpElectricity)
+
+  //function that will reflect the current values of units available
+  function updateDisplay() {
+    document.getElementById('unitsAvailable').innerHTML= unitsAvailable;
+    document.getElementById('totalUnits').innerHTML = totalUnits;
+    document.getElementById('totalAmount').innerHTML = totalAmount;
+  
+    if (advanceUsed) {
+      document.getElementById('advanceTaken').innerHTML = '✔';
+    } else {
+      document.getElementById('advanceTaken').innerHTML = '';
+    }
+  }
+
+  // this function will handle the usage of appliances, checking if there are enough units 
+  function useAppliance(units, appliance) {
+    var appliance = document.querySelector("input[name='useElectricity']:checked");
+    if (unitsAvailable >= units) {
+        unitsAvailable -= units;
+      updateDisplay();
+      alert(`Appliance used successfully.`);
+    } else {
+      alert(`Not enough units available to use this appliance.`);
+    }
+  }
+  useButton.addEventListener("click", useAppliance);
+  
+
+  //this function will handle paying back the advance amount
+  function payBackAdvance() {
+    advanceUsed = false;
+    updateDisplay();
+  }
+
+
+  
--- electricity.js
+++ electricity.js
@@ -1,20 +1,39 @@
 function Electricity() {
+    let unitsAvailable = 0;
+    let advanceHasTaken = false;
+    const advanceAmount = 30;
+    const advanceUnits = 21;
+    let advancePaid = false;
+    let totalAmount = 0;
+    let unitBought = 0;
 
-    // do we want to go with this or array? 
-    let appliances = {
-        'Stove': 10, 
-        'Kettle': 5, 
-        'TV': 3, 
-        'Fridge': 13
-    };
+
+   
 
     function topUpElectricity(amount) {
-
+        if (amount === 10) {
+            unitsAvailable += 7;
+            totalAmount += 10;
+            unitBought += 7
+        } else if (amount === 20) {
+            unitsAvailable += 14;
+            totalAmount += 20;
+            unitBought += 14
+        } else if (amount === 50) {
+            unitsAvailable += 35;
+            totalAmount += 50;
+            unitBought += 35;
+        } else if (amount === 'advance' && !advanceHasTaken) {
+            unitsAvailable += advanceUnits;
+            advancePaid = false
+            advanceHasTaken = true;
+            unitBought += advanceUnits
+        }
 
     }
 
     function getUnitsAvailable() {
-         return unitsAvailable;
+        return unitsAvailable;
     }
 
     /*
@@ -22,16 +41,41 @@ function Electricity() {
     * other wise return false and do nothing.
     */
     function useAppliance(appliance) {
-        
+        let unitsNeeded = 0;
+
+        switch (appliance) {
+            case 'TV':
+                unitsNeeded = 3;
+                break;
+            case 'Kettle':
+                unitsNeeded = 5;
+                break;
+            case 'Fridge':
+                unitsNeeded = 13;
+                break;
+            case 'Stove':
+                unitsNeeded = 10;
+                break;
+        }
+        if (unitsAvailable >= unitsNeeded) {
+            unitsAvailable -= unitsNeeded;
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    function advanceTaken() {
+    function advanceTaken(amount) {
+        return advanceHasTaken && !advancePaid;
+      
     }
 
     function totalAmountSpent() {
+        return totalAmount;
     }
 
-    function totalUnitsBought(){
+    function totalUnitsBought() {
+        return unitBought;
     }
 
     return {
@@ -44,3 +88,11 @@ function Electricity() {
 
     }
 }
+
+ // do we want to go with this or array? 
+ let appliances = {
+    'Stove': 10,
+    'Kettle': 5,
+    'TV': 3,
+    'Fridge': 13
+};
